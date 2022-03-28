d359cde7fd6ade61b2e9e3cf820a51930d4ac837
redx1t <68523591+redx1t@users.noreply.github.com>
2022-03-29 00:52:27 +0300

    get request params

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
+
+app.get("/products/:id", (req, res) => {
+  res.json(product.find(() => {}));
+});
+
+//catch 404
 app.all("*", (req, res) => {
-  res.status(200).send("Not found");
+  res.status(404).send("Not found");
 });
 //all requests
 app.listen(5000, () => {
-  console.log("check");
+  console.log("listening to port 5000");
 });
