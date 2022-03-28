6f00ca83aa1b0fac4acf5a9270a8a403207cf5e8
redx1t <68523591+redx1t@users.noreply.github.com>
2022-03-29 01:12:08 +0300

    middleware setup

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
 const app = express();
-
+const logger = (req, res, next) => {
+  console.log(req.url);
+  next();
+};
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
@@ -10,14 +13,14 @@ app.get("/products/:id", (req, res) => {
   res.json(product.find(() => {}));
 });
 
-app.get("/products", (req, res) => {
+app.get("/products", logger, (req, res) => {
   //access query parameters
   console.log(req.query);
   res.json(product.find(() => {}));
 });
 
 //catch 404
-app.all("*", (req, res) => {
+app.all("*", logger, (req, res) => {
   res.status(404).send("Not found");
 });
 //all requests
